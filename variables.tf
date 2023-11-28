5dfb447b6821648c1489aada0cf6d7452fccd713
AJ <ajarosinski@gmail.com>
2023-11-28 16:32:12 -0600

    Enhance capability to add min/max instances (#1)

--- variables.tf
+++ variables.tf
@@ -7,3 +7,21 @@ EOF
   type    = map(string)
   default = {}
 }
+
+variable "minimum_instances" {
+  type        = number
+  default     = 1
+  description = <<EOF
+The minimum number of instances that you want.
+See https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/using-features.managing.as.html for details on autoscaling
+EOF
+}
+
+variable "maximum_instances" {
+  type        = number
+  default     = 4
+  description = <<EOF
+The maximum number of instances that you want.
+See https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/using-features.managing.as.html for details on autoscaling
+EOF
+}
